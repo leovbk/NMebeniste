b13b16509c1d1db04bae22d710b370a25c206df2
leov <leo.verbeke@hotmail.fr>
2022-04-27 18:56:32 +0200

    refactor: gtag plugin syntax

--- plugins/gtag.js
+++ plugins/gtag.js
@@ -1,11 +1,6 @@
 import Vue from 'vue'
 import VueGtag from 'vue-gtag'
-import App from './App.vue'
 
 Vue.use(VueGtag, {
   config: { id: 'G-1Q2RH4J4TJ' },
 })
-
-new Vue({
-  render: (h) => h(App),
-}).$mount('#app')
